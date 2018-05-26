518c3cdcf59fd236366e8ff7590e45b7bfd647c4
marksnyder <mark@snyderis.com>
2018-05-26 16:19:24 -0500

    create web service project

---
+++ Service/Controllers/TimelineController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Service.Controllers
+{
+    [Produces("application/json")]
+    [Route("v1/Timeline")]
+    public class TimelineController : Controller
+    {
+        [HttpGet]
+        public IDictionary<string, string> GetEntityState(string entityid, string on)
+        {
+            return null;
+        }
+    }
+}
